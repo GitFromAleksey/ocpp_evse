65bef14d86ba254a1e0af7a1598ee44c28d16fba
Aleksey <a_h.79@mail.ru>
2023-08-05 00:05:38 +0700

    - добавлен класс для работы с файлом настроек

--- main.py
+++ main.py
@@ -51,28 +51,136 @@ from charge_point import ChargePoint
 #  - Ошибка во время передачи сообщений. 
 #  - Сообщение было получено, но формат был неправильным.
 
+CHARGE_POINT_NAME = 'FMA'
+CHARGE_POINT_VENDOR = 'NSTU'
 SCHEMAS_FOLDER = '\schemas\json'
 SERVER_IP = '192.168.1.20'
 SERVER_PORT = '8180'
 SERVER_WS_ADDRESS = 'ws://' + SERVER_IP + ':' + SERVER_PORT + '/steve/websocket/CentralSystemService/FMA'
 #'ws://192.168.1.20:8180/steve/websocket/CentralSystemService/' + CHARGE_POINT_NAME
+SETTINGS_FILE_NAME = 'settings.json'
+
+SETTINGS = {
+            'SERVER_IP' : SERVER_IP, 
+            'SERVER_PORT' : SERVER_PORT, 
+            'CHARGE_POINT_NAME' : CHARGE_POINT_NAME,
+            # 'CHARGE_POINT_VENDOR' : CHARGE_POINT_VENDOR,
+            }
+
+def GetSettings():
+    from pathlib import Path
+    global SETTINGS
+    file_exist = Path(SETTINGS_FILE_NAME)
+    if file_exist.is_file():
+        print(f'exist')
+        file = open(SETTINGS_FILE_NAME, 'r', encoding = "utf-8")
+    else:
+        print(f'not exist')
+        file = open(SETTINGS_FILE_NAME, 'w', encoding = "utf-8")
+        json_settings = json.dumps(SETTINGS, indent = 2)
+        file.write(json_settings)
+        file.close()
+        file = open(SETTINGS_FILE_NAME, 'r', encoding = "utf-8")
+
+    try:
+        load_settings = json.loads(file.read())
+        SETTINGS = load_settings
+    except:
+        file.close()
+        file = open(SETTINGS_FILE_NAME, 'w', encoding = "utf-8")
+        json_settings = json.dumps(SETTINGS, indent = 2)
+        file.write(json_settings)
+
+    file.close()
+
+class Settings:
+    CHARGE_POINT_NAME = 'ChargePointName'
+    CHARGE_POINT_VENDOR = 'Vendor'
+    SCHEMAS_FOLDER = '\schemas\json'
+    SERVER_IP = '192.168.1.20'
+    SERVER_PORT = '8180'
+    SETTINGS_FILE_NAME = 'settings.json'
+    SETTINGS = {
+                'SERVER_IP' : SERVER_IP, 
+                'SERVER_PORT' : SERVER_PORT, 
+                'CHARGE_POINT_NAME' : CHARGE_POINT_NAME,
+                # 'CHARGE_POINT_VENDOR' : CHARGE_POINT_VENDOR,
+                }
+
+    def __init__(self, path: str = '') -> None:
+        self.path = Settings.SETTINGS_FILE_NAME # path + '\\' + Settings.SETTINGS_FILE_NAME
+        print(self.path)
+        self.settings = Settings.SETTINGS
+        if self.IsFileExist(self.path):
+            if self.ReadSettingsFromFile(self.path) == False:
+                self.CreateNewFile(self.path)
+        else:
+            self.CreateNewFile(self.path)
+
+    def IsFileExist(self, path: str) -> bool:
+        from pathlib import Path
+        file_exist = Path(path)
+        if file_exist.is_file():
+            print(f'exist')
+            return True
+        else:
+            print(f'not exist')
+            return False
+
+    def CreateNewFile(self,  path: str):
+        file = open(path, 'w', encoding = "utf-8")
+        json_settings = json.dumps(Settings.SETTINGS, indent = 2)
+        file.write(json_settings)
+        file.close()
+        file = open(path, 'r', encoding = "utf-8")
+
+    def ReadSettingsFromFile(self, path: str) -> bool:
+        res = True
+        file = open(path, 'r', encoding = "utf-8")
+        try:
+            load_settings = json.loads(file.read())
+            self.settings = load_settings
+            # self.FileDataIsValid(path)
+        except:
+            res = False
+        file.close()
+        return res
+
+    def FileDataIsValid(self) -> bool:
+        pass
+
+    def GetChargePointName(self) -> str:
+        return self.settings['CHARGE_POINT_NAME']
+    def GetWsServerAddress(self) -> str:
+        addr = 'ws://' 
+        addr += self.settings['SERVER_IP'] 
+        addr += ':' 
+        addr += self.settings['SERVER_PORT']
+        addr += '/steve/websocket/CentralSystemService/' 
+        addr += self.GetChargePointName()
+        return addr
+
 
 async def main():
-    cwd = os.getcwd()
-    schemas_dir = cwd + SCHEMAS_FOLDER
-    print(f'schemas_dir: {schemas_dir}')
-    schemas_files = [f for f in os.listdir(schemas_dir) if os.path.isfile(os.path.join(schemas_dir, f))]
-    # print(f'{schemas_files}')
-    if 'Authorize.json' in schemas_files:
-        path = schemas_dir + '\Authorize.json'
-        with open(path, 'rt') as f:
-            Authorize = json.load(f)
-            f.close()
+    # GetSettings()
+    cp_settings = Settings()
+    print(cp_settings.GetWsServerAddress())
 
-    print(f'{Authorize.get("properties").get("idTag")}')
+    # cwd = os.getcwd()
+    # schemas_dir = cwd + SCHEMAS_FOLDER
+    # print(f'schemas_dir: {schemas_dir}')
+    # schemas_files = [f for f in os.listdir(schemas_dir) if os.path.isfile(os.path.join(schemas_dir, f))]
+    # # print(f'{schemas_files}')
+    # if 'Authorize.json' in schemas_files:
+    #     path = schemas_dir + '\Authorize.json'
+    #     with open(path, 'rt') as f:
+    #         Authorize = json.load(f)
+    #         f.close()
 
-    async with ws.connect(SERVER_WS_ADDRESS, subprotocols=['ocpp1.6']) as socket:
-        cp = ChargePoint('FMA', socket)
+    # print(f'{Authorize.get("properties").get("idTag")}')
+
+    async with ws.connect(cp_settings.GetWsServerAddress(), subprotocols=['ocpp1.6']) as socket:
+        cp = ChargePoint(cp_settings.GetChargePointName(), socket)
         await asyncio.gather(
             cp.start(),
             cp.BootNotificationSend()
